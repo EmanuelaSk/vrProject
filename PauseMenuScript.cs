1c0fec138e1e611edf76c62b94acd8986ec80c20
Comp0sator <Quarterinchspam@yahoo.com>
2018-03-22 10:43:43 -0400

    Add files via upload

---
+++ PauseMenuScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour {
+
+	public Transform canvas;
+	public Transform canvasWin;
+	public static bool pauseGame;
+	public static bool paused;
+	protected float delay;
+
+	AudioSource[] sounds;
+	AudioSource music;
+
+	void Start  () {
+		delay = 1;
+		pauseGame = false;
+		Time.timeScale = 1;
+		sounds = this.GetComponents<AudioSource> ();
+		music = sounds [0];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if((Input.GetKeyDown(KeyCode.Escape)) && (pauseGame == false)){
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0;
+				music.Pause ();
+				paused = true;
+			} 
+			else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+				music.UnPause ();
+				paused = false;
+			}
+		}
+		if (pauseGame)
+			music.Pause ();
+		else
+			music.UnPause ();
+	}
+
+	public void Resume()
+	{
+		canvas.gameObject.SetActive (false);
+		Time.timeScale = 1;
+		music.UnPause ();
+		pauseGame = false;
+	}
+
+	public void Replay()
+	{
+		SceneManager.LoadScene ("Map");
+	}
+
+	public void Quit()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+}
---
+++ TimerScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+	public Text counterText;
+	public float timeStamp;
+	public float setTime = 180;
+	public bool usingTimer = false;
+	public Transform canvasLose;
+	//public static bool endGame;
+
+	// Use this for initialization
+	void Start () {
+		//endGame = false;
+		SetTimer (setTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (usingTimer)
+			SetUIText ();
+	}
+
+	public void SetTimer(float time) {
+		if (usingTimer)
+			return;
+
+		timeStamp = Time.time + time;
+		usingTimer = true;
+	}
+
+	public void SetUIText() {
+		float timeLeft = timeStamp - Time.time;
+		if (timeLeft <= 0) {
+			FinishedCount ();
+			return;
+		}
+		float hours;
+		float minutes;
+		float seconds;
+		float miliseconds;
+		GetTimeValues (timeLeft, out hours, out minutes, out seconds, out miliseconds);
+
+		if (hours > 0)
+			counterText.text = string.Format ("{0}:{1}", hours, minutes);
+		else if (minutes > 0)
+			counterText.text = string.Format ("{0}:{1}", minutes, seconds);
+		else if (seconds > 0)
+			counterText.text = string.Format ("{0}:{1}", seconds, miliseconds);
+		
+	}
+
+	public void GetTimeValues(float time, out float hours, out float minutes, out float seconds, out float miliseconds) {
+		hours = (int)(time / 3600f);
+		minutes = (int)((time - hours * 3600f) / 60f);
+		seconds = (int)((time - hours * 3600f - minutes * 60f));
+		miliseconds = (int)((time - hours * 3600f - minutes * 60f - seconds) * 100f);
+	}
+
+	public void FinishedCount() {
+		Debug.Log("Timer End");
+		counterText.text = "00:00";
+		usingTimer = false;
+		canvasLose.gameObject.SetActive (true);
+		Time.timeScale = 0;
+		PauseMenuScript.pauseGame = true;
+	}
+}
