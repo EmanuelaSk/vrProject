801d354b81be80aa1b9a9f781b66c8a955dcb79d
unknown <Tech@C401IT38.gbc.local>
2018-03-22 15:47:03 -0400

    pre-Alpha
    merged group files, all mechanics are not fully implemented yet

---
+++ vr_master/Assets/_Scripts/PlayerCollision.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    CapsuleCollider plBody;
+    // Use this for initialization
+    //GoldPickup treasure;
+    int scoreValue = 15;
+    bool score = false;
+
+    void Awake()
+    {
+
+        plBody = GetComponent<CapsuleCollider>();
+        //   GoldPickup treasure = treasure.GetComponent<Collider>();
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        // Increase the score by the enemy's score value.
+        score = true;
+    }
+
+
+    private void Update()
+    {
+        if (score)
+        {
+            ScoreManager.score += scoreValue;
+            score = false;
+        }
+    }
+
+
+}
+
+    // Update is called once per frame
+    //void Update () {
+    //    // Perform the raycast against gameobjects on the shootable layer and if it hits something...
+    //    if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+    //    {
+    //        // Try and find an EnemyHealth script on the gameobject hit.
+    //        GoldPickup goldCollide = goldCollide.collider.GetComponent<>
+    //            //plBody.collider.GetComponent<isH>();
+
+    //        // If the EnemyHealth component exist...
+    //        if (enemyHealth != null)
+    //        {
+    //            // ... the enemy should take damage.
+    //            enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+    //        }
+
+    //        // Set the second position of the line renderer to the point the raycast hit.
+    //        gunLine.SetPosition(1, shootHit.point);
+
+
+    //    }
+    //}
